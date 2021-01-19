12ef21f9e3a81446ee95f74e0dece1dff82a026f
Nihad Keric <nihadkeric@gmail.com>
2021-01-19 13:50:32 +0100

    convert to pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { FlipCardComponent } from './components/flip-card/flip-card.component';
 import { CardFrontComponent } from './components/card-front/card-front.component';
 import { CardBackComponent } from './components/card-back/card-back.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
 	declarations: [ AppComponent, FlipCardComponent, CardFrontComponent, CardBackComponent ],
-	imports: [ BrowserModule, BrowserAnimationsModule ],
+	imports: [ BrowserModule, BrowserAnimationsModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }) ],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
